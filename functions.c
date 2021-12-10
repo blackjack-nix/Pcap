448948eec92c1d9501eebaf5c82a4127c94a06ca
blackjack-nix <theo.peresse-gourbil@edu.esiee.fr>
2021-12-10 11:42:13 +0100

    help + list all devices

--- functions.c
+++ functions.c
@@ -17,3 +17,26 @@ char * detect_device() {
     printf("Network device found: %s\n", device);
     return device;
 }
+
+void detect_all_devices(){
+	pcap_if_t *all_devices;
+	char * device;
+	char error_buffer[PCAP_ERRBUF_SIZE];
+
+	pcap_findalldevs(&all_devices, error_buffer);
+
+	printf("%s ",all_devices -> name);
+
+	while (1){
+		printf(" %s",all_devices->name);
+		all_devices = all_devices->next;
+		if (all_devices->next == NULL)
+		{
+			return;
+		}
+	}
+}
+
+void print_help(){
+	printf("Welcome on sniffer based on pcap lib \nUse ./sniffer -h to display this help. \nAll parameters : \n-L : To list all interfaces available to sniff on;");
+}
--- main.c
+++ main.c
@@ -1,12 +1,24 @@
+#include <string.h>
+
 #include "functions.h"
 #include "functions.c"
 
-int main(char * argc, char * argv[]){
-	char * device = detect_device();
-	if (device == NULL){
+int main(int argc, char * argv[]){
+	
+	if(argc == 1){
+		printf("Use ./sniffer to print help");
 		return 1;
 	}
 
+	if(strcmp(argv[1],"-h") == 0 && argc == 2){
+		print_help();
+		return 0;
+	}
+
+	if(strcmp(argv[1],"-L") == 0 && argc == 2){
+		detect_all_devices();
+		return 0;
+	}
 
 
 }
