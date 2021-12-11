e056a1ba1f1f4ba946a5371bfbe885956c1487f3
blackjack-nix <theo.peresse-gourbil@edu.esiee.fr>
2021-12-11 23:46:16 +0100

    improuve help and sudo features

--- sniffer.c
+++ sniffer.c
@@ -6,12 +6,6 @@
 
 int main(int argc, char * argv[]){
 	
-
-	if(argc == 1){
-		printf("Use ./sniffer -h to print help");
-		return 1;
-	}
-
 	if(strcmp(argv[1],"-h") == 0 && argc == 2){
 		print_help();
 		return 0;
@@ -22,23 +16,34 @@ int main(int argc, char * argv[]){
 		return 0;
 	}
 
-	int user = checkSudo();
-	if (user != 0){
-		return 1;
-	}
+	
 
 	if(strcmp(argv[1],"-t") == 0 && argc == 3){
+		int user = checkSudo();
+		if (user != 0){
+			return 1;
+		}
+
 		open_device(argv[2]);
 		return 0;
 	}
 
 	if(strcmp(argv[1],"-i") == 0 && argc == 3){
+		int user = checkSudo();
+		if (user != 0){
+			return 1;
+		}
+
 		u_char * paquet = intercept_paquet(argv[2]);
 		if(paquet == NULL){
 			return 1;
 		}
 		return 0;
 	}
+
+	printf("Use ./sniffer -h to print help");
+	return 0;
+
 
 
 }
